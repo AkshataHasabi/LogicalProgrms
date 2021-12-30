83260d59405b04997b6cdd59b0b8bf682ca1f84e
AkshataHasabi <akshuh818@gmail.com>
2021-12-30 23:20:06 +0530

    ADDED:calculated series of a numbers

--- LogicalProgram.java
+++ LogicalProgram.java
@@ -1,8 +1,26 @@
 package com.bridgelab.logicalprograms;
 
+import java.util.Scanner;
+
 public class LogicalProgram {
+	public static int FirstNum1;
+	public static int SecondNum2;
+	public static int Number;
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Logical Programms");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the First number:");
+		FirstNum1=sc.nextInt();
+		System.out.print("Enter the Second number:");
+		SecondNum2=sc.nextInt();
+		System.out.print("Enter the fibonacci series number:");
+		Number=sc.nextInt();
+		System.out.println("Fibonacci Series Of " + Number + " is:"+" " + FirstNum1 + ","+ SecondNum2);
+		for(int i = 1; i<Number; i++) {
+			int nextNumber = FirstNum1 + SecondNum2;
+			FirstNum1=SecondNum2;
+			SecondNum2=nextNumber;
+			System.out.println(nextNumber);
+		}
 	}
 }
